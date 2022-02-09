5b91d62be865742280c7ce0cf017ca17b0494366
bsaoudi <bilel.seoudi@gmail.com>
2022-02-09 17:49:30 +0100

    format code

--- src/main/java/com/gkemayo/library/loan/LoanId.java
+++ src/main/java/com/gkemayo/library/loan/LoanId.java
@@ -2,7 +2,6 @@ package com.gkemayo.library.loan;
 
 import com.gkemayo.library.book.Book;
 import com.gkemayo.library.customer.Customer;
-import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -11,21 +10,52 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Embeddable
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
 public class LoanId implements Serializable {
 
 
     private static final long serialVersionUID = 3912193101593832821L;
 
-    @ManyToOne
     private Book book;
 
-    @ManyToOne
     private Customer customer;
 
-    @Column(name = "CREATION_DATE_TIME")
     private LocalDateTime creationDateTime;
+
+    public LoanId() {
+        super();
+    }
+
+    public LoanId(Book book, Customer customer) {
+        super();
+        this.book = book;
+        this.customer = customer;
+        this.creationDateTime = LocalDateTime.now();
+    }
+
+    @ManyToOne
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book bbok) {
+        this.book = bbok;
+    }
+
+    @ManyToOne
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Column(name = "CREATION_DATE_TIME")
+    public LocalDateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(LocalDateTime creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
 }
