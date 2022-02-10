aad03d2535da14a0f096ea000e5a5a387bb2a4b9
bsaoudi <bilel.seoudi@gmail.com>
2022-02-10 11:14:06 +0100

    Business logic layer

--- src/main/java/com/gkemayo/library/category/ICategoryDao.java
+++ src/main/java/com/gkemayo/library/category/ICategoryDao.java
@@ -1,4 +1,8 @@
 package com.gkemayo.library.category;
 
-public interface ICategoryDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICategoryDao extends JpaRepository<Category, Integer> {
 }
